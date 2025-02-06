0df1babb5626873897e76820742fce7a4a3ebb70
Zachery Proctor <zachery.proctor@principia.edu>
2025-02-06 06:55:57 -0600

    "Adds triangle fillings and gravity"

--- js/dots.js
+++ js/dots.js
@@ -13,6 +13,8 @@ const canvas = document.querySelector("#myCanvas");
     const RADIUS = 2;
     let dots = [];
     const DIST_MAX = 50;
+    const GRAVITY = .1;
+    const FRICTON = .9999;
 
     function Dot() {
         this.x = Math.random() * canvas.width;
@@ -33,27 +35,45 @@ const canvas = document.querySelector("#myCanvas");
         ctx.fill();
 
         if (this.x < 0 || this.x > canvas.width){
+            this.x = this.x > 0 ? canvas.width : 0;
             this.dx *= -1;
         }
         if (this.y < 0 || this.y > canvas.height){
+            this.y = this.y > 0 ? canvas.height : 0;
             this.dy *= -1;
         }
 
         for (let i = 0; i<dots.length; i++){
+            dots[i].dx *= FRICTON;
+            dots[i].dy *= FRICTON;
             for (let j=i+1; j<dots.length; j++){
-                const distance = Math.sqrt((dots[i].x-dots[j].x)**2, (dots[i].y-dots[j].y)**2)
+                const dx = dots[j].x - dots[i].x;
+                const dy = dots[j].y - dots[i].y;
+                const distance = Math.sqrt(dx**2 + dy**2)
                 if (distance < DIST_MAX){
+                    
                     ctx.beginPath();
                     ctx.moveTo(dots[i].x, dots[i].y);
                     ctx.lineTo(dots[j].x, dots[j].y);
                     ctx.strokeStyle = "rgba(255,100,200, " + (1 - distance/DIST_MAX) + ")";
                     ctx.lineWidth = 1 - distance/DIST_MAX;
                     ctx.stroke();
+                    
+                    //add a gravitational attraction between dots that are closer than DIST_MAX to each other.
+                    
+                    const angle = Math.atan2(dy, dx);
+                    dots[i].dx += (Math.cos(angle)/2) * (1 - distance/DIST_MAX) * GRAVITY;
+                    dots[i].dy += (Math.sin(angle)/2) * (1 - distance/DIST_MAX) * GRAVITY;
+                    dots[j].dx -= (Math.cos(angle)/2) * (1 - distance/DIST_MAX) * GRAVITY;
+                    dots[j].dy -= (Math.sin(angle)/2) * (1 - distance/DIST_MAX) * GRAVITY;
+                    
+
                     for (let k = j+1; k<dots.length; k++){
-                        const k_i_dist = Math.sqrt((dots[k].x-dots[i].x)**2, (dots[k].y-dots[i].y)**2);
-                        const k_j_dist = Math.sqrt((dots[k].x-dots[j].x)**2, (dots[k].y-dots[j].y)**2);
+                        const k_i_dist = Math.sqrt((dots[k].x-dots[i].x)**2 + (dots[k].y-dots[i].y)**2);
+                        const k_j_dist = Math.sqrt((dots[k].x-dots[j].x)**2 + (dots[k].y-dots[j].y)**2);
                         if (k_i_dist < DIST_MAX && k_j_dist < DIST_MAX){
                             perimtotal = distance + k_i_dist + k_j_dist;
+                            //This fill val looks a little weird, I just think the top range of alphas all look really similar.
                             fillVal = .67 - (perimtotal < DIST_MAX*2 ? perimtotal : DIST_MAX*2)/(DIST_MAX*3);
                             ctx.beginPath();
                             ctx.moveTo(dots[i].x, dots[i].y);
@@ -64,6 +84,7 @@ const canvas = document.querySelector("#myCanvas");
                             ctx.fill();
                         }
                     }
+                        
                 }
             }
         }
